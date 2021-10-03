857722534b173d403be0a1ed7a1467d9ed591e23
Sai Teja Erukude <erukude.saiteja@gmail.com>
2021-10-03 13:29:34 +0530

    Added Cart functionality

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,37 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    const item = action.payload
+    
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            const existItem = state.cartItems.find(
+                (x) => x.product === item.product
+            )
+
+            if (existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map((x) =>
+                        x.product === item.product ? item : x
+                    ),
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item],
+                }
+            }
+
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    (x) => x.product !== item
+                ),
+            }
+
+        default:
+            return state
+    }
+}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,91 +1,122 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Card, Image, ListGroup, Button } from 'react-bootstrap'
+import { Row, Col, Card, Image, ListGroup, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 
 import { useDispatch, useSelector } from 'react-redux'
-import { listProducts } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listProductDetails } from '../actions/productActions'
 
+const ProductScreen = ({ history, match }) => {
+   const [qty, setQty] = useState(1)
 
+   const dispatch = useDispatch()
 
-const ProductScreen = ({ match }) => {
-    const dispatch = useDispatch()
+   const productDetails = useSelector((state) => state.productDetails)
+   const { loading, error, product } = productDetails
 
-    const productDetails = useSelector(state => state.productDetails)
-    const { loading, error, product } = productDetails
+   useEffect(() => {
+      dispatch(listProductDetails(match.params.id))
+   }, [dispatch, match])
 
-    useEffect(() => {
-        dispatch(listProductDetails(match.params.id))
-    }, [dispatch, match])
+   const addToCartHandler = () => {
+      history.push(`/cart/${match.params.id}?qty=${qty}`)
+   }
 
-    return (
-        <>
-            <Link className="btn btn-dark my-3" to="/">
-                Go Back
-            </Link>
-            {
-                loading ? (
-                    <Loader />
-                ) : error ? (
-                    <Message variant='danger'>{error}</Message>
-                ) : (
-                    <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid></Image>
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant="flush">
+   return (
+      <>
+         <Link className='btn btn-dark my-3' to='/'>
+            Go Back
+         </Link>
+         {loading ? (
+            <Loader />
+         ) : error ? (
+            <Message variant='danger'>{error}</Message>
+         ) : (
+            <Row>
+               <Col md={6}>
+                  <Image src={product.image} alt={product.name} fluid></Image>
+               </Col>
+               <Col md={3}>
+                  <ListGroup variant='flush'>
+                     <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                     </ListGroup.Item>
+                     <ListGroup.Item>
+                        <Rating
+                           value={product.rating}
+                           text={product.numReviews}
+                        />
+                     </ListGroup.Item>
+                     <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                     <ListGroup.Item>
+                        Description: {product.description}
+                     </ListGroup.Item>
+                  </ListGroup>
+               </Col>
+               <Col md={3}>
+                  <Card>
+                     <ListGroup variant='flush'>
                         <ListGroup.Item>
-                            <h3>{product.name}</h3>
+                           <Row>
+                              <Col>Price: </Col>
+                              <Col>
+                                 <h5>${product.price}</h5>
+                              </Col>
+                           </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Rating value={product.rating} text={product.numReviews} />
+                           <Row>
+                              <Col>Status: </Col>
+                              <Col>
+                                 {product.countInStock > 0
+                                    ? 'In Stock'
+                                    : 'Out Of Stock'}
+                              </Col>
+                           </Row>
                         </ListGroup.Item>
+
+                        {product.countInStock > 0 && (
+                           <ListGroup.Item>
+                              <Row>
+                                 <Col>Qty: </Col>
+                                 <Col>
+                                    <Form.Control
+                                       as='select'
+                                       value={qty}
+                                       onChange={(e) => setQty(e.target.value)}
+                                    >
+                                       {[
+                                          ...Array(product.countInStock).keys(),
+                                       ].map((x) => (
+                                          <option key={x + 1} value={x + 1}>
+                                             {x + 1}
+                                          </option>
+                                       ))}
+                                    </Form.Control>
+                                 </Col>
+                              </Row>
+                           </ListGroup.Item>
+                        )}
+
                         <ListGroup.Item>
-                            Price: ${product.price}
+                           <Button
+                              className='btn-block w-100'
+                              type='button'
+                              disabled={product.countInStock <= 0}
+                              onClick={addToCartHandler}
+                           >
+                              Add To Cart
+                           </Button>
                         </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Price: </Col>
-                                    <Col><h5>${product.price}</h5></Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Status: </Col>
-                                    <Col>
-                                        {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button
-                                    className="btn-block w-100"
-                                    type="button"
-                                    disabled={product.countInStock <= 0}>
-                                    Add To Cart
-                                </Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
+                     </ListGroup>
+                  </Card>
+               </Col>
             </Row>
-                )
-            }
-            
-        </>
-    )
+         )}
+      </>
+   )
 }
 
 export default ProductScreen
