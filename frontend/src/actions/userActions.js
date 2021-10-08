9c7f6e56ba3693490a496e852177f335d1bf084f
Sai Teja Erukude <erukude.saiteja@gmail.com>
2021-10-08 10:48:17 +0530

    User List backend and frontend

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,6 +13,10 @@ import {
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_DETAILS_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET,
 } from '../constants/userConstants'
 import axios from '../axios'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
@@ -63,6 +67,9 @@ const logout = () => (dispatch) => {
     })
     dispatch({
         type: CART_RESET_ITEMS,
+    })
+    dispatch({
+        type: USER_LIST_RESET,
     })
 }
 
@@ -172,4 +179,34 @@ const updateUserProfile = (user) => async (dispatch, getState) => {
     }
 }
 
-export { login, logout, register, getUserDetails, updateUserProfile }
+const listUsers = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_LIST_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.get('/api/users', config)
+
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_LIST_FAIL,
+            payload: error.message,
+        })
+    }
+}
+
+export { login, logout, register, getUserDetails, updateUserProfile, listUsers }
