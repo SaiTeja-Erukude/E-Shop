841862bf265ce84edfd21a175b2c31246033399c
Sai Teja Erukude <erukude.saiteja@gmail.com>
2021-10-10 11:34:11 +0530

    Ready for deployment

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://e-shop-app-77.herokuapp.com',
 })
 
 export default instance
-// http://e-shop-app-77.herokuapp.com
